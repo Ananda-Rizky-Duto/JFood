c08a64e37b8f5306a6f30c5eb4e8ed5ced3f337f
Ananda-Rizky-Duto <ananda.rizky71@ui.ac.id>
2020-03-30 13:13:49 +0700

    Modul 6 PreCS

---
+++ src/CashInvoice.java
@@ -0,0 +1,89 @@
+/**
+ * Kelas untuk membentuk invoice dengan pembayaran Cash
+ *
+ * @author Ananda Rizky Duto Pamungkas
+ * @version 10 Maret 2020
+ */
+
+import java.util.*;
+import java.text.*;
+import java.text.SimpleDateFormat;  
+import java.util.Date; 
+
+public class CashInvoice extends Invoice
+{
+    private static final PaymentType PAYMENT_TYPE = PaymentType.Cash;
+    private int deliveryFee;
+    
+    public CashInvoice(int id, Food food, Customer customer, InvoiceStatus invoiceStatus)
+    {
+        super(id, food, customer, invoiceStatus);
+    }
+    
+    public CashInvoice(int id, Food food, Customer customer, InvoiceStatus invoiceStatus, int deliveryFee)
+    {
+        super(id, food, customer, invoiceStatus);
+        this.deliveryFee = deliveryFee;
+    }
+    
+    public PaymentType getPaymentType()
+    {
+        return PAYMENT_TYPE;
+    }
+    
+    public int getDeliveryFee()
+    {
+        return deliveryFee;
+    }
+    
+    public void setDeliveryFee(int deliveryFee)
+    {
+        this.deliveryFee = deliveryFee;
+    }
+    
+    public void setTotalPrice()
+    {
+        if(this.deliveryFee > 0)
+        {
+            this.totalPrice = getFood().getPrice() + this.deliveryFee;
+        }
+        
+        else
+        {
+            this.totalPrice = getFood().getPrice();
+    
+        }
+    }
+    
+    public String toString()
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy");
+        if(this.deliveryFee > 0)
+        {
+            return "==========INVOICE==========\n\n" +
+                   "ID : " + getId() +
+                   "\nFood : " + getFood().getName() +
+                   "\nDate : " + sdf.format(super.getDate().getTime()) +
+                   "\nCustomer : " + getCustomer().getName() +
+                   "\nDelivery Fee : " + getDeliveryFee() +
+                   "\nTotal price : " + getTotalPrice() +
+                   "\nStatus : " + getInvoiceStatus() +
+                   "\nPayment Type : " + getPaymentType() +
+                   "\n";
+        }
+        
+        else
+        {
+            return "==========INVOICE==========\n\n" +
+                   "ID : " + getId() +
+                   "\nFood : " + getFood().getName() +
+                   "\nDate : " + sdf.format(super.getDate().getTime()) +
+                   "\nCustomer : " + getCustomer().getName() +
+                   "\nDelivery Fee : Free Delivery" +
+                   "\nTotal price : " + getTotalPrice() +
+                   "\nStatus : " + getInvoiceStatus() +
+                   "\nPayment Type : " + getPaymentType() +
+                   "\n";
+        }
+    }
+}
---
+++ src/DatabaseSeller.java
@@ -0,0 +1,32 @@
+/**
+ * Kelas ini digunakan sebagai pengatur database penjual pada JFood.
+ * 
+ * @author Ananda Rizky Duto Pamungkas
+ * @version 5 Maret 2020
+ * 
+ */
+
+public class DatabaseSeller
+{
+    private static String[] listSeller;
+    
+    public static boolean addSeller(Seller seller)
+    {
+        return false;
+    }
+    
+    public static boolean removeSeller(Seller seller)
+    {
+        return false;
+    }
+    
+    public static Seller getSeller()
+    {
+        return null;
+    }
+    
+    public static String[] getListSeller()
+    {
+        return listSeller;
+    }
+}
